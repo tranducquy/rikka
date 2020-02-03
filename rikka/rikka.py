c8551f84715ea1f2a446956e14a4fac31277c8d8
1011.mbp2016 <takeyuki.t@gmail.com>
2020-02-04 08:18:04 +0900

    20200204

--- rikka/rikka.py
+++ rikka/rikka.py
@@ -2,22 +2,39 @@
 # -*- coding: utf-8 -*-
 
 
-import time
 import os
+import time
+import json
 import sys
 import jpxpy.realtime_index
+from logging import config, getLogger
+
+
+def logger_init(name='test'):
+    config.dictConfig(json.loads(open("log.json", encoding='UTF-8').read()))
+    return getLogger(name)
 
 
 def main_unit():
     loop_sec = 60
-    short_stop_value = 819.00
+    short_stop_value = 816.00
     long_stop_value = 0.0
+    logger = logger_init()
+    logger.info(f"rikka: long_stop_value:[{long_stop_value}],short_stop_value:[{short_stop_value}]")
     while True:
-        mothers = jpxpy.realtime_index.get_realtime_index_mothers()
-        low_price = mothers["low_price"]
-        if short_stop_value > low_price:
-            print(mothers["low_price_time"] + ":" + mothers["low_price"])
-
+        try:
+            mothers = jpxpy.realtime_index.get_realtime_index_mothers()
+            close_price_time = mothers["close_price_time"]
+            close_price = mothers["close_price"]
+            if close_price and long_stop_value != 0 and long_stop_value < close_price:
+                logger.info(f"LONG ORDER close_price:[{close_price},({close_price_time})]")
+            elif close_price and short_stop_value != 0 and short_stop_value > close_price:
+                logger.info(f"SHORT ORDER close_price:[{close_price},({close_price_time})]")
+            else:
+                logger.info(f"NONE ORDER close_price:[{close_price}({close_price_time})]"
+                            f",stop_order_value:[{long_stop_value}],stop_short_value:[{short_stop_value}]")
+        except Exception as err:
+            logger.error(err)
         time.sleep(loop_sec)
 
 
@@ -25,7 +42,7 @@ def daemonize():
     pid = os.fork()
     if pid > 0:
         pid_file = open('/tmp/rikka_daemon.pid', 'w')
-        pid_file.write(str(pid)+"\n")
+        pid_file.write(str(pid) + "\n")
         pid_file.close()
         sys.exit()
     if pid == 0:
@@ -36,4 +53,3 @@ if __name__ == '__main__':
     # while True:
     #     daemonize()
     main_unit()
-
